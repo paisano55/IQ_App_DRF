f89b9c0dbbf69f0d75131bcaca84264d2c110f21
Kim HyunMin <paisano55@naver.com>
2020-04-15 13:48:45 +0900

    model

--- quote_maker/models.py
+++ quote_maker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CPU(models.Model):
+    maker = models.CharField(max_length=10)
+    gen = models.CharField(max_length=20) #세대
+    socket = models.CharField(max_length=20) #소켓
+    core = models.IntegerField(max_length=3)
+    thread = models.IntegerField(max_length=3)
