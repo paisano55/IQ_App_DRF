3a62bcb6bab6aaa0c4c73cefacfd9f9da6b564f4
Kim HyunMin <paisano55@naver.com>
2020-04-22 16:23:23 +0900

    serializer

--- quote_maker/serializers.py
+++ quote_maker/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
 from .models import CPU,MB,RAM,VGA,SSD,HDD,CASE,PSU,Quote
 
-class 
+class CpuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CPU
+        fields = ('')
